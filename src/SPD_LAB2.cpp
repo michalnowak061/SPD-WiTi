c343d7f118f0c5166a50532f9d7cd379e30a8540
Mariusz Perskawiec <maniek777777@interia.eu>
2019-03-17 19:17:06 +0100

    Utworzenie repo

---
+++ src/SPD_LAB2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SPD_LAB2.cpp
+// Author      : Mariusz
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Dzia³amy!!!" << endl; // prints !!!Dzia³amy!!!
+	return 0;
+}
